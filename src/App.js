a7cc2101ff29cdde472de8998de57090fceb4575
Larry McCullough <larry.mccullough88@gmail.com>
2022-11-06 00:05:42 -0400

    Header and Footer Links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-
+import {Routes,Route} from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage'
 import Header from "./components/Header";
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -9,7 +11,11 @@ function App() {
   return (
     <div className="App">
             <Header/>
-            <CampsitesDirectoryPage  />
+            <Routes>
+               <Route path='/' element={<HomePage/>} />
+               <Route path='contact' element={<ContactPage/>} />
+               <Route path='directory' element={<CampsitesDirectoryPage/>} />
+            </Routes>
             <Footer />
     </div>
   );
